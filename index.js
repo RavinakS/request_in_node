80b7b04f14558c07aee62bf0aa6dae7f5d281f5a
RavinakS <ravinasingh697@gmail.com>
2021-08-25 19:45:29 +0530

    #@#@

--- index.js
+++ index.js
@@ -4,13 +4,26 @@ const fs = require('fs');
 const url = 'http://saral.navgurukul.org/api/courses';
 const fileName = 'courses.json';
 
-function storeData(filename){
-    return new Promise(resolve, reject){
-        
-    }
+function storeData(filename, data){
+    fs.writeFile(filename, data, 'utf8', (err, data)=>{
+        if(err){
+            console.log(err);
+        }
+        console.log("Successfully Written.");
+    })
+    // return new Promise(resolve, reject){
+    //     fs.writeFile(filename, data, 'utf8', (err, data)=>{
+    //         if(err){
+    //             console.log(err);
+    //         }
+    //         console.log("Successfully Written.");
+    //     })
+    // }
 }
 
 axios.get(url).then((response)=>{
-    let data = response;
-    console.log(data.data);
+    data = JSON.stringify(response, null, 2);
+    storeData(fileName, data)
+}).then((done)=>{
+    console.log("Hi");
 })
