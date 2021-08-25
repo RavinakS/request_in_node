f7ebb628431606f56ead0525c92d30d3e4c39218
RavinakS <ravinasingh697@gmail.com>
2021-08-25 19:32:36 +0530

    ***

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const axios = require('axios')
+const fs = require('fs');
 
 const url = 'http://saral.navgurukul.org/api/courses';
+const fileName = 'courses.json';
+
+function storeData(filename){
+    return new Promise(resolve, reject){
+        
+    }
+}
 
 axios.get(url).then((response)=>{
     let data = response;
