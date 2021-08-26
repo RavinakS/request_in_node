fd71e5ce5df755941e0be4b8fb6aeeef7f51cb51
RavinakS <ravinasingh697@gmail.com>
2021-08-26 17:06:15 +0530

    written the data in the json file

--- index.js
+++ index.js
@@ -5,25 +5,26 @@ const url = 'http://saral.navgurukul.org/api/courses';
 const fileName = 'courses.json';
 
 function storeData(filename, data){
-    fs.writeFile(filename, data, 'utf8', (err, data)=>{
-        if(err){
-            console.log(err);
-        }
-        console.log("Successfully Written.");
-    })
-    // return new Promise(resolve, reject){
-    //     fs.writeFile(filename, data, 'utf8', (err, data)=>{
-    //         if(err){
-    //             console.log(err);
-    //         }
-    //         console.log("Successfully Written.");
-    //     })
-    // }
-}
+    // fs.writeFile(filename, data, 'utf8', (err, data)=>{
+    //     if(err){
+    //         console.log(err);
+    //     }
+    //     console.log("Successfully Written.");
+    // })
+    return new Promise((resolve, reject)=>{
+        fs.writeFile(filename, data, 'utf8', (err, data)=>{
+            if(err){
+                console.log(err);
+            }
+            console.log("Successfully Written.");
+        })
+    }
+    )}
 
 axios.get(url).then((response)=>{
-    data = JSON.stringify(response, null, 2);
-    storeData(fileName, data)
-}).then((done)=>{
+    data = JSON.stringify(response.data, null, 2);
+    return storeData(fileName, data);
+}).then((courses)=>{
+    // data = JSON.stringify(courses.data, null, 2)
     console.log("Hi");
 })
