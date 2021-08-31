6e2fd9a8baa7f827ba0129c2ac2ec165e480c735
RavinakS <ravinasingh697@gmail.com>
2021-08-31 12:21:41 +0530

    using asyncAwait

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+const { rejects } = require('assert');
 const axios = require('axios')
 const fs = require('fs');
+const { resolve } = require('path');
 
 const url = 'http://saral.navgurukul.org/api/courses';
 const fileName = 'courses.json';
 
 function storeData(filename, data){
-    // fs.writeFile(filename, data, 'utf8', (err, data)=>{
-    //     if(err){
-    //         console.log(err);
-    //     }
-    //     console.log("Successfully Written.");
-    // })
     return new Promise((resolve, reject)=>{
-        fs.writeFile(filename, data, 'utf8', (err, data)=>{
-            if(err){
-                console.log(err);
-            }
-            console.log("Successfully Written.");
-        })
-    }
-    )}
+        const string_data = JSON.stringify(data.data, null, 2);
 
-axios.get(url).then((response)=>{
-    data = JSON.stringify(response.data, null, 2);
-    return storeData(fileName, data);
-}).then((courses)=>{
-    // data = JSON.stringify(courses.data, null, 2)
-    console.log("Hi");
-})
+        fs.writeFile(filename, string_data, 'utf8', (err) =>{
+            if(err){
+                reject("something went wrong!");
+            }else{
+                resolve("Written successfully!")
+            }
+        })
+    })
+}
+
+async function start(){
+    const data = await axios.get(url);
+
+    const wrote = await storeData(fileName, data);
+    console.log(wrote);
+
+    
+}
+
+start();
